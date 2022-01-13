9ffd6ac4cdb1137790d3f6e8d0199e138e87c639
张国栋 <zhangguodong@songguo7.com>
2022-01-13 18:12:06 +0800

    新建测试模块spring-demo

---
+++ spring-demo/src/main/java/org/zgd/spring/demo/Main.java
@@ -0,0 +1,17 @@
+package org.zgd.spring.demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author zhangguodong
+ * @since 2022/1/13 17:29
+ */
+public class Main {
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		Person obj = (Person) context.getBean("person");
+		System.out.println(obj.getName());
+	}
+}
---
+++ spring-demo/src/main/java/org/zgd/spring/demo/Person.java
@@ -0,0 +1,19 @@
+package org.zgd.spring.demo;
+
+/**
+ * @author zhangguodong
+ * @since 2022/1/13 17:28
+ */
+public class Person {
+	private String name;
+
+	public String getName() {
+
+		return name;
+	}
+
+	public void setName(String name) {
+
+		this.name = name;
+	}
+}
